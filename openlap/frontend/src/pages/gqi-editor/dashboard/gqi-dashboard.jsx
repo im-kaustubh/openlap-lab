634aac8cd4f43dc23703b62d75bba75f566ee1c9
imKaustubh29 <51872311+im-kaustubh@users.noreply.github.com>
2025-06-04 15:43:26 +0200

    created a buttton

--- openlap/frontend/src/pages/gqi-editor/dashboard/gqi-dashboard.jsx
+++ openlap/frontend/src/pages/gqi-editor/dashboard/gqi-dashboard.jsx
@@ -1,3 +1,4 @@
+/*
 import { Divider, Grid, Typography } from "@mui/material";
 import React from "react";
 
@@ -10,6 +11,38 @@ const GQIDashboard = () => {
         </Grid>
         <Grid item xs={12}>
           <Divider />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+export default GQIDashboard;
+*/
+import { Divider, Grid, Typography, Button } from "@mui/material"; // Import Button
+import React, { useState } from "react"; // Import useState
+
+const GQIDashboard = () => {
+  // Create a state and initialize with string
+  const [myState, setMyState] = useState("initial value");
+
+  // Function to console log the value of the state
+  const logStateValue = () => {
+    console.log(myState);
+  };
+
+  return (
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography>GQI Dashboard</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Divider />
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant="contained" onClick={logStateValue}>
+            this is a button
+          </Button>
         </Grid>
       </Grid>
     </>
